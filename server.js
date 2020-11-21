1b0611d36d2fbb3abc3d192285840f167417a9d7
tohsic-7 <tohsic-7@student.ltu.se>
2020-11-21 11:31:45 +0100

    Manager and prosumer GET/UPDATE/DELETE complete
    first iteration of get, update and delete methods for managers and prosumers are completed

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var Consumer = require('./models/consumer');
 var Manager = require('./models/manager');
 
 
-mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology:true});
+mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology:true, useFindAndModify:true});
 
 
 // Construct a schema, using GraphQL schema language
@@ -19,6 +19,8 @@ var schema = buildSchema(`
     hello: String
     getOneProsumer(id: Int): Prosumer
     getProsumers: [Prosumer]
+    getOneManager(id: Int): Manager
+    getManagers: [Manager]
     getOneConsumer(id: Int): Consumer
     getConsumers: [Consumer]
   }
@@ -40,6 +42,18 @@ var schema = buildSchema(`
     consumption: Int
   }
 
+  type Manager{
+    id: Int,
+    buffer: Int,
+    consumption: Int,
+    production: Int,
+    status: Boolean,
+    ratio: Int,
+    demand: Int,
+    price: Int,
+    img_path: String
+  }
+
 type Mutation{
   insertProsumer(
     id: Int,
@@ -52,6 +66,7 @@ type Mutation{
     online: Boolean,
     img_path: String):Prosumer
   
+  deleteProsumer(id:Int!): Prosumer
 
   updateProsumer(
     id: Int,
@@ -63,6 +78,30 @@ type Mutation{
     ratio_under: Int,
     online: Boolean,
     img_path: String):Prosumer
+
+  insertManager(
+    id: Int,
+    buffer: Int,
+    consumption: Int,
+    production: Int,
+    status: Boolean,
+    ratio: Int,
+    demand: Int,
+    price: Int,
+    img_path: String): Manager
+  
+  deleteManager(id:Int!): Manager
+
+  updateManager(
+    id: Int,
+    buffer: Int,
+    consumption: Int,
+    production: Int,
+    status: Boolean,
+    ratio: Int,
+    demand: Int,
+    price: Int,
+    img_path: String):Manager
 
   insertConsumer(
     id: Int,
@@ -80,9 +119,6 @@ type Mutation{
  
 // The root provides a resolver function for each API endpoint
 var root = {
-  hello: () => { 
-    return 'Hejsan';
-  },
   getConsumers: ()=> {
     values = Consumer.find();
     return values;
@@ -105,9 +141,9 @@ var root = {
 
   // Prosumer query resolvers
   getOneProsumer: (args)=> {
-    values = Prosumer.findOne({ id: args.id });
-    console.log(values);
-  
+    values = Prosumer.findOne({ id: args.id }, function(err){
+      if(err) return console.error(err);
+    });  
     return values;
   },
   getProsumers: ()=> {
@@ -116,7 +152,7 @@ var root = {
     });
     return values;
   },
-
+  //Prosumer mutation resolvers
   insertProsumer: (args) => {
     var newProsumer = new Prosumer({
       id: args.id,
@@ -135,24 +171,105 @@ var root = {
     return newProsumer;
   },
 
-  updateProsumer: (args) => {
-    values = Prosumer.findOne({ id: args.id });
-    console.log(typeof values);
-    values.buffer = args.buffer
-    values.save(function(err, prosumer){
-      if (err) return console.error(err);
-    })
-  },
-  
-  insertConsumer: (args) => {
-    var consumer = new Consumer({id: args.id, consumption: args.consumption});
-    consumer.save(function(err, result){
+  deleteProsumer: (args) => {
+    filter = {id: args.id};
+    deadProsumer = Prosumer.deleteOne(filter, function(err){
       if (err) return console.error(err);
     });
-  }
+    return deadProsumer;
+  },
+
+  updateProsumer: (args) => {
+    var filter = { id: args.id };
+    var update = {
+      buffer: args.buffer,
+      wind: args.wind,
+      consumption: args.consumption,
+      production: args.production,
+      ratio_excess: args.ratio_excess,
+      ratio_under: args.ratio_under,
+      online: args.online,
+      img_path: args.img_path
+    }
+    // Only update the non-null & defined values
+    for(var property in update){
+      if (update[property] === null || update[property] === undefined) {
+        delete update[property];
+      }
+    }
+    values = Prosumer.findOneAndUpdate(filter, update, {new:true}, function(err, prosumer){
+      if (err) return console.error(err);
+    });
+    return values;
+  },
+
+  // Manager query resolvers
+  getOneManager: (args)=> {
+    values = Manager.findOne({ id: args.id }, function(err){
+      if(err) return console.error(err);
+    });  
+    return values;
+  },
+  getManagers: ()=> {
+    var values = Manager.find(function(err, result){
+      if(err) return console.error(err);
+    });
+    return values;
+  },
+  //Manager mutation resolvers
+  insertManager: (args) => {
+    var newManager = new Manager({
+      id: args.id,
+      buffer: args.buffer,
+      consumption: args.consumption,
+      production: args.production,
+      status: args.status,
+      ratio: args.ratio,
+      demand: args.demand,
+      price: args.price,
+      img_path: args.img_path
+    });
+    newManager.save(function(err, manager){
+      if (err) return console.error(err);
+    })
+    return newManager;
+  },
+
+  deleteManager: (args) => {
+    filter = {id: args.id};
+    deadManager = Manager.deleteOne(filter, function(err){
+      if (err) return console.error(err);
+    });
+    return deadManager;
+  },
+
+  updateManager: (args) => {
+    var filter = { id: args.id };
+    var update = {
+      buffer: args.buffer,
+      consumption: args.consumption,
+      production: args.production,
+      status: args.status,
+      ratio: args.ratio,
+      demand: args.demand,
+      price: args.price,
+      img_path: args.img_path
+    }
+    // Only update the non-null & defined values
+    for(var property in update){
+      if (update[property] === null || update[property] === undefined) {
+        delete update[property];
+      }
+    }
+    values = Manager.findOneAndUpdate(filter, update, {new:true}, function(err, prosumer){
+      if (err) return console.error(err);
+    });
+    return values;
+  },
+  
 };
 
- 
+// server setup
 var app = express();
 app.use('/graphql', graphqlHTTP({
   schema: schema,
